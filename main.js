611a11d92e1c424412c2f482c08d981c466cfbac
helloparthshah <helloparthshah@gmail.com>
2021-01-17 00:53:17 +0530

    Eye tracking

--- main.js
+++ main.js
@@ -140,3 +140,92 @@ const mouse = {
 }
 
 document.onmousemove = mouse.handleMouseMove;
+
+let currentModel;
+
+function createModel() {
+    const model = tf.sequential();
+
+    model.add(tf.layers.conv2d({
+        kernelSize: 5,
+        filters: 20,
+        strides: 1,
+        activation: 'relu',
+        inputShape: [$('#eyes').height(), $('#eyes').width(), 3],
+    }));
+
+    model.add(tf.layers.maxPooling2d({
+        poolSize: [2, 2],
+        strides: [2, 2],
+    }));
+
+    model.add(tf.layers.flatten());
+
+    model.add(tf.layers.dropout(0.2));
+
+    // Two output values x and y
+    model.add(tf.layers.dense({
+        units: 2,
+        activation: 'tanh',
+    }));
+
+    // Use ADAM optimizer with learning rate of 0.0005 and MSE loss
+    model.compile({
+        optimizer: tf.train.adam(0.0005),
+        loss: 'meanSquaredError',
+    });
+
+    return model;
+}
+
+function fitModel() {
+    let batchSize = Math.floor(dataset.train.n * 0.1);
+    if (batchSize < 4) {
+        batchSize = 4;
+    } else if (batchSize > 64) {
+        batchSize = 64;
+    }
+
+    if (currentModel == null) {
+        currentModel = createModel();
+    }
+
+    currentModel.fit(dataset.train.x, dataset.train.y, {
+        batchSize: batchSize,
+        epochs: 20,
+        shuffle: true,
+        validationData: [dataset.val.x, dataset.val.y],
+    });
+    console.log("Done!")
+}
+
+$('#train').click(function() {
+    fitModel();
+});
+
+function moveTarget() {
+    if (currentModel == null) {
+        return;
+    }
+    tf.tidy(function() {
+        const image = getImage();
+        const prediction = currentModel.predict(image);
+
+        // Convert normalized position back to screen position:
+        const targetWidth = $('#target').outerWidth();
+        const targetHeight = $('#target').outerHeight();
+
+        // It's okay to run this async, since we don't have to wait for it.
+        prediction.data().then(prediction => {
+            const x = ((prediction[0] + 1) / 2) * ($(window).width() - targetWidth);
+            const y = ((prediction[1] + 1) / 2) * ($(window).height() - targetHeight);
+
+            // Move target there:
+            const $target = $('#target');
+            $target.css('left', x + 'px');
+            $target.css('top', y + 'px');
+        });
+    });
+}
+
+setInterval(moveTarget, 100);
