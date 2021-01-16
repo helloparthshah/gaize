84717a4bda5357cb494eb445b50a974adbc92eee
helloparthshah <helloparthshah@gmail.com>
2021-01-17 00:42:27 +0530

    Eyes detection working

--- main.js
+++ main.js
@@ -16,6 +16,32 @@ $(document).ready(function() {
         if (currentPosition) {
             ctrack.draw(overlay);
         }
+
+        if (currentPosition) {
+            // Draw facial mask on overlay canvas:
+            ctrack.draw(overlay);
+
+            // Get the eyes rectangle and draw it in red:
+            const eyesRect = getEyesRectangle(currentPosition);
+            overlayCC.strokeStyle = 'red';
+            overlayCC.strokeRect(eyesRect[0], eyesRect[1], eyesRect[2], eyesRect[3]);
+
+            // The video might internally have a different size, so we need these
+            // factors to rescale the eyes rectangle before cropping:
+            const resizeFactorX = video.videoWidth / video.width;
+            const resizeFactorY = video.videoHeight / video.height;
+
+            // Crop the eyes from the video and paste them in the eyes canvas:
+            const eyesCanvas = $('#eyes')[0];
+            const eyesCC = eyesCanvas.getContext('2d');
+
+            eyesCC.drawImage(
+                video,
+                eyesRect[0] * resizeFactorX, eyesRect[1] * resizeFactorY,
+                eyesRect[2] * resizeFactorX, eyesRect[3] * resizeFactorY,
+                0, 0, eyesCanvas.width, eyesCanvas.height
+            );
+        }
     }
 
     function onStreaming(stream) {
@@ -26,3 +52,15 @@ $(document).ready(function() {
 
     navigator.mediaDevices.getUserMedia({ video: true }).then(onStreaming);
 });
+
+function getEyesRectangle(positions) {
+    const minX = positions[23][0] - 5;
+    const maxX = positions[28][0] + 5;
+    const minY = positions[24][1] - 5;
+    const maxY = positions[26][1] + 5;
+
+    const width = maxX - minX;
+    const height = maxY - minY;
+
+    return [minX, minY, width, height];
+}
