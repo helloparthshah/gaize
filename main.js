21ce5c00d930196d2b2ff854350be5ba976756a3
helloparthshah <helloparthshah@gmail.com>
2021-01-17 00:22:35 +0530

    Face detection works

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
 $(document).ready(function() {
     const video = $('#webcam')[0];
+    const ctrack = new clm.tracker();
+    ctrack.init();
+
+    const overlay = $('#overlay')[0];
+    const overlayCC = overlay.getContext('2d');
+
+    function trackingLoop() {
+
+        requestAnimationFrame(trackingLoop);
+
+        let currentPosition = ctrack.getCurrentPosition();
+        overlayCC.clearRect(0, 0, 400, 300);
+
+        if (currentPosition) {
+            ctrack.draw(overlay);
+        }
+    }
 
     function onStreaming(stream) {
         video.srcObject = stream;
+        ctrack.start(video);
+        trackingLoop()
     }
 
     navigator.mediaDevices.getUserMedia({ video: true }).then(onStreaming);
