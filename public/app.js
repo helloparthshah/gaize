90d8143e00a91a99c161afbb6da745f58d7a97a3
helloparthshah <helloparthshah@gmail.com>
2021-02-10 02:59:00 +0530

    Added heatmap

--- public/app.js
+++ public/app.js
@@ -1,3 +1,65 @@
+// var body = $('body');
+// var hmEl = $('.heatmap-wrapper');
+
+// hmEl.css('width', body.width());
+// hmEl.css('height', '100vh');
+
+var hm = document.querySelector('.heatmap');
+
+var heatmap = h337.create({
+    container: hm,
+    radius: 60
+});
+
+
+var trackData = false;
+
+setInterval(function() {
+    trackData = true;
+}, 50);
+
+var idleTimeout, idleInterval;
+
+var lastX, lastY;
+var idleCount;
+
+function startIdle() {
+    idleCount = 0;
+
+    function idle() {
+        heatmap.addData({
+            x: lastX,
+            y: lastY
+        });
+        idleCount++;
+        if (idleCount > 10) {
+            clearInterval(idleInterval);
+        }
+    };
+    idle();
+    idleInterval = setInterval(idle, 1000);
+};
+
+
+
+/* document.onmousemove = function(ev) {
+    // console.log(ev)
+    if (idleTimeout) clearTimeout(idleTimeout);
+    if (idleInterval) clearInterval(idleInterval);
+
+    if (trackData) {
+        lastX = ev.clientX;
+        lastY = ev.clientY;
+        heatmap.addData({
+            x: lastX,
+            y: lastY
+        });
+        trackData = false;
+    }
+    idleTimeout = setTimeout(startIdle, 500);
+}; */
+
+
 const video = $('#webcam')[0];
 const ctrack = new clm.tracker();
 ctrack.init();
@@ -233,6 +295,21 @@ function moveTarget() {
             const $target = $('#target');
             $target.css('left', x + 'px');
             $target.css('top', y + 'px');
+
+            // Add data to the heatmap
+            if (idleTimeout) clearTimeout(idleTimeout);
+            if (idleInterval) clearInterval(idleInterval);
+
+            if (trackData) {
+                lastX = x;
+                lastY = y;
+                heatmap.addData({
+                    x: lastX,
+                    y: lastY
+                });
+                trackData = false;
+            }
+            idleTimeout = setTimeout(startIdle, 500);
         });
     });
 }
